4be78696c9c52d3487e7863bd81c9a2f3172576c
Yadnyesh Bharat Juvekar <yadnyesh.juvekar@gmail.com>
2017-05-28 03:51:32 +0530

    Updated RequestFilter class

--- src/main/java/com/bookstore/config/RequestFilter.java
+++ src/main/java/com/bookstore/config/RequestFilter.java
@@ -1,5 +1,9 @@
 package com.bookstore.config;
 
+import javax.servlet.FilterChain;
+import javax.servlet.ServletRequest;
+import javax.servlet.ServletResponse;
+
 import org.springframework.core.Ordered;
 import org.springframework.core.annotation.Order;
 import org.springframework.stereotype.Component;
@@ -9,5 +13,7 @@ import org.springframework.stereotype.Component;
 public class RequestFilter {
 	
 	public void doFilter(ServletRequest req, ServletResponse res, FilterChain chain) {
+		
+	}
 
 }
