6f282587de8f3aae3e9a93a4fc1dd3af8771848c
Yadnyesh Bharat Juvekar <yadnyesh.juvekar@gmail.com>
2017-05-27 20:46:39 +0530

    Lecture 17

--- src/main/java/com/bookstore/domain/security/Authority.java
+++ src/main/java/com/bookstore/domain/security/Authority.java
@@ -2,8 +2,11 @@ package com.bookstore.domain.security;
 
 import java.io.Serializable;
 
+import javax.persistence.Entity;
+
 import org.springframework.security.core.GrantedAuthority;
 
+@Entity
 public class Authority implements GrantedAuthority, Serializable{
 	private static final long serialVersionUID = 123123L;
 	
