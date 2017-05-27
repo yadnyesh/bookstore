0f453dea5b33f1177708b37a07859679f76beb4b
Yadnyesh Bharat Juvekar <yadnyesh.juvekar@gmail.com>
2017-05-27 17:26:35 +0530

    Modified UserRole

--- src/main/java/com/bookstore/domain/security/UserRole.java
+++ src/main/java/com/bookstore/domain/security/UserRole.java
@@ -34,6 +34,30 @@ public class UserRole implements Serializable{
 		this.user = user;
 		this.role = role;
 	}
+
+	public User getUser() {
+		return user;
+	}
+
+	public void setUser(User user) {
+		this.user = user;
+	}
+
+	public Role getRole() {
+		return role;
+	}
+
+	public void setRole(Role role) {
+		this.role = role;
+	}
+
+	public Long getUserRoleId() {
+		return userRoleId;
+	}
+
+	public void setUserRoleId(Long userRoleId) {
+		this.userRoleId = userRoleId;
+	}
 	
 	
 	
