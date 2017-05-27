620001c48a95c03165f2bb748d54e8ac60bf40fc
Yadnyesh Bharat Juvekar <yadnyesh.juvekar@gmail.com>
2017-05-27 17:30:19 +0530

    Added User file

--- src/main/java/com/bookstore/domain/User.java
+++ src/main/java/com/bookstore/domain/User.java
@@ -3,11 +3,17 @@ package com.bookstore.domain;
 import java.io.Serializable;
 
 import javax.persistence.Entity;
+import javax.persistence.GeneratedValue;
+import javax.persistence.GenerationType;
+import javax.persistence.Id;
 
 @Entity
 public class User implements Serializable{
 
 	private static final long serialVersionUID = 112398147L;
+	
+	@Id
+	@GeneratedValue(strategy = GenerationType.AUTO)
 	
 	private Long id;
 
