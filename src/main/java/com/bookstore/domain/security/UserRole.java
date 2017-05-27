636d8ff571783e055a9796990f72458cb5369d98
Yadnyesh Bharat Juvekar <yadnyesh.juvekar@gmail.com>
2017-05-27 16:34:26 +0530

    Modified UserRole

--- src/main/java/com/bookstore/domain/security/UserRole.java
+++ src/main/java/com/bookstore/domain/security/UserRole.java
@@ -1,10 +1,18 @@
 package com.bookstore.domain.security;
 
+import java.io.Serializable;
+
 import javax.persistence.Entity;
+import javax.persistence.Id;
 import javax.persistence.Table;
 
 @Entity
 @Table(name="user_role")
-public class UserRole {
+public class UserRole implements Serializable{
+
+	private static final long serialVersionUID = 800000L;
+	
+	@Id
+	private Long userRoleId;
 	
 }
