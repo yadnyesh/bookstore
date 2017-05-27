657f64780cb64cb745ae261b7ea16fd145347bb3
Yadnyesh Bharat Juvekar <yadnyesh.juvekar@gmail.com>
2017-05-27 20:24:25 +0530

    Modified Security Service

--- src/main/java/com/bookstore/service/UserSecurityService.java
+++ src/main/java/com/bookstore/service/UserSecurityService.java
@@ -2,6 +2,7 @@ package com.bookstore.service;
 
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.security.core.userdetails.UserDetailsService;
 import org.springframework.stereotype.Service;
 
@@ -9,4 +10,15 @@ import org.springframework.stereotype.Service;
 public class UserSecurityService implements UserDetailsService{
 		private static final Logger LOG = LoggerFactory.getLogger(UserSecurityService.class);
 		
+		@Autowired 
+		private UserRepository userRepository;
+		
+		public UserDetails loadUserByUsername(String username) throws UsernameNotFoundException {
+			User user = userRepository.findByUsername(username);
+			if (user == null) {
+				LOG.warn("Username {} not found");
+				throw new UsernameNotFoundException("Username " +  username + " not found");
+			}
+			return user;
+		}
 }
