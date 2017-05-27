140a127c335dbe9fba5a15fd01a0ebd01d6a660e
Yadnyesh Bharat Juvekar <yadnyesh.juvekar@gmail.com>
2017-05-27 17:49:14 +0530

    Added SecurityConfig

--- src/main/java/com/bookstore/config/SecurityConfig.java
+++ src/main/java/com/bookstore/config/SecurityConfig.java
@@ -1,5 +1,12 @@
 package com.bookstore.config;
 
-public class SecurityConfig {
+import org.springframework.context.annotation.Configuration;
+import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
+import org.springframework.security.config.annotation.web.configuration.WebSecurityConfigurerAdapter;
 
+@Configuration
+@EnableWebSecurity
+public class SecurityConfig extends WebSecurityConfigurerAdapter{
+
+	
 }
