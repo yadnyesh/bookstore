100f637a272b169162b1670169d88726a14afe9a
Yadnyesh Bharat Juvekar <yadnyesh.juvekar@gmail.com>
2017-05-27 17:34:07 +0530

    Added User file

--- src/main/java/com/bookstore/domain/User.java
+++ src/main/java/com/bookstore/domain/User.java
@@ -1,12 +1,18 @@
 package com.bookstore.domain;
 
 import java.io.Serializable;
+import java.util.HashSet;
+import java.util.Set;
 
+import javax.persistence.CascadeType;
 import javax.persistence.Column;
 import javax.persistence.Entity;
 import javax.persistence.GeneratedValue;
 import javax.persistence.GenerationType;
 import javax.persistence.Id;
+import javax.persistence.OneToMany;
+
+import com.bookstore.domain.security.UserRole;
 
 @Entity
 public class User implements Serializable{
@@ -18,5 +24,15 @@ public class User implements Serializable{
 	@Column(name="Id", nullable=false, updatable=false)
 	private Long id;
 
+	private String username;
+	private String password;
+	private String firstName;
+	private String lastName;
 	
+	private String email;
+	private String phone;
+	private boolean enabled = true;
+	
+	@OneToMany(mapped = "user", cascade=CascadeType.ALL)
+	private Set<UserRole> userRoles = new HashSet<>();
 }
