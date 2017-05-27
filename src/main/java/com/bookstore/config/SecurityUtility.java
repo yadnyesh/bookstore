1740c96022f5a97e1c366f8bbf5a1ba5eea9b01f
Yadnyesh Bharat Juvekar <yadnyesh.juvekar@gmail.com>
2017-05-27 20:00:41 +0530

    Modified Security Utility

--- src/main/java/com/bookstore/config/SecurityUtility.java
+++ src/main/java/com/bookstore/config/SecurityUtility.java
@@ -1,6 +1,10 @@
 package com.bookstore.config;
 
+import org.springframework.stereotype.Component;
 
+@Component
 public class SecurityUtility {
+	
+	private static final String SALT = "salt"; // should be protected carefully
 
 }
