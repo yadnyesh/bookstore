220bbde9063abbd5840b5c36bda0ce0ba4305fc3
Yadnyesh Bharat Juvekar <yadnyesh.juvekar@gmail.com>
2017-05-27 20:17:42 +0530

    Modified Security Utility

--- src/main/java/com/bookstore/service/UserSecurityService.java
+++ src/main/java/com/bookstore/service/UserSecurityService.java
@@ -1,5 +1,12 @@
 package com.bookstore.service;
 
-public class UserSecurityService {
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+import org.springframework.security.core.userdetails.UserDetailsService;
+import org.springframework.stereotype.Service;
 
+@Service
+public class UserSecurityService implements UserDetailsService{
+		private static final Logger LOG = LoggerFactory.getLogger(UserSecurityService.class);
+		
 }
