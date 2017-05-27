2b5197d9ac301e4457a7ee69186ae9efaecc826f
Yadnyesh Bharat Juvekar <yadnyesh.juvekar@gmail.com>
2017-05-27 21:21:12 +0530

    Too many changes

--- src/main/java/com/bookstore/BookstoreAngularApplication.java
+++ src/main/java/com/bookstore/BookstoreAngularApplication.java
@@ -3,6 +3,7 @@ package com.bookstore;
 import java.util.HashSet;
 import java.util.Set;
 
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.CommandLineRunner;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
@@ -11,10 +12,14 @@ import com.bookstore.config.SecurityUtility;
 import com.bookstore.domain.User;
 import com.bookstore.domain.security.Role;
 import com.bookstore.domain.security.UserRole;
+import com.bookstore.service.UserService;
 
 @SpringBootApplication
 public class BookstoreAngularApplication implements CommandLineRunner{
 
+	@Autowired
+	private UserService userService;
+	
 	public static void main(String[] args) {
 		SpringApplication.run(BookstoreAngularApplication.class, args);
 	}
--- src/main/java/com/bookstore/impl/UserServiceImpl.java
+++ src/main/java/com/bookstore/impl/UserServiceImpl.java
@@ -4,28 +4,42 @@ import java.util.Set;
 
 import javax.transaction.Transactional;
 
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
 
 import com.bookstore.domain.User;
 import com.bookstore.domain.security.UserRole;
+import com.bookstore.repository.RoleRepository;
 import com.bookstore.repository.UserRepository;
 import com.bookstore.service.UserService;
 
 @Service
 public class UserServiceImpl implements UserService{
 
+	private static final Logger LOG = LoggerFactory.getLogger(UserService.class);
+	
 	@Autowired
 	private UserRepository userRepository;
 	
-
+	@Autowired
+	private RoleRepository roleRepository;
+	
 	@Override
 	@Transactional
 	public User createUser(User user, Set<UserRole> userRoles) {
 		User localUser = userRepository.findByUsername(user.getUsername());
 		if(localUser != null) {
-			LOG.info("User with username {} already exist. Nothing will be done"), user.getUsername());
+			LOG.info("User with username {} already exist. Nothing will be done", user.getUsername());
+		} else {
+			for (UserRole ur: userRoles) {
+				roleRepository.save(ur.getRole());
+			}
+			user.getUserRoles().addAll(userRoles);
+			localUser = userRepository.save(user);
 		}
+		 return localUser;
 	}
 	
 	
