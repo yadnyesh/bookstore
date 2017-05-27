f68a92aa39127f7633ea186dd15ce44606ce41d7
Yadnyesh Bharat Juvekar <yadnyesh.juvekar@gmail.com>
2017-05-28 04:12:11 +0530

    Updated RequestFilter class

--- src/main/java/com/bookstore/config/RequestFilter.java
+++ src/main/java/com/bookstore/config/RequestFilter.java
@@ -30,6 +30,12 @@ public class RequestFilter {
 			} catch (Exception e){
 				e.printStackTrace();
 			}
+		} else {
+			System.out.println("Pre-flight");
+			response.setHeader("Access-Control-Allowed-Methods", "POST, GET, DELETE");
+			response.setHeader("Access-Control-Max-Age", "3600");
+			response.setHeader("Access-Control-Allow-Headers", "authorization, content-type, x-auth-token, " + 
+								"access-control-request-headers, access-control-request-method, accept, origin, authorization,x-requested-with");
 		}
 		
 	}
