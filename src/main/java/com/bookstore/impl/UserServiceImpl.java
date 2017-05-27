243ccb69f9322b09c2d9189cdd354ba95e410b0b
Yadnyesh Bharat Juvekar <yadnyesh.juvekar@gmail.com>
2017-05-27 21:12:18 +0530

    Adding more files

--- src/main/java/com/bookstore/impl/UserServiceImpl.java
+++ src/main/java/com/bookstore/impl/UserServiceImpl.java
@@ -1,11 +1,14 @@
 package com.bookstore.impl;
 
+import java.util.Set;
+
 import javax.transaction.Transactional;
 
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
 
 import com.bookstore.domain.User;
+import com.bookstore.domain.security.UserRole;
 import com.bookstore.repository.UserRepository;
 import com.bookstore.service.UserService;
 
@@ -15,7 +18,15 @@ public class UserServiceImpl implements UserService{
 	@Autowired
 	private UserRepository userRepository;
 	
+
+	@Override
 	@Transactional
-	public User createUser(User user, Set<UserRole> userRoles);
+	public User createUser(User user, Set<UserRole> userRoles) {
+		User localUser = userRepository.findByUsername(user.getUsername());
+		if(localUser != null) {
+			LOG.info("User with username {} already exist. Nothing will be done"), user.getUsername());
+		}
+	}
+	
 	
 }
