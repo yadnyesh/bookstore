6d5c8fc91ae9830493b72056179c26f1134ebc9a
Yadnyesh Bharat Juvekar <yadnyesh.juvekar@gmail.com>
2017-05-27 20:57:33 +0530

    Added CommandLineRunner

--- src/main/java/com/bookstore/BookstoreAngularApplication.java
+++ src/main/java/com/bookstore/BookstoreAngularApplication.java
@@ -1,12 +1,28 @@
 package com.bookstore;
 
+import org.springframework.boot.CommandLineRunner;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
+import com.bookstore.domain.User;
+
 @SpringBootApplication
-public class BookstoreAngularApplication {
+public class BookstoreAngularApplication implements CommandLineRunner{
 
 	public static void main(String[] args) {
 		SpringApplication.run(BookstoreAngularApplication.class, args);
 	}
+
+	@Override
+	public void run(String... arg0) throws Exception {
+		User user1 = new User();
+		user1.setFirstName("Yadnyesh");
+		user1.setLastName("BharatJuvekar");
+		user1.setUsername("yb");
+		
+
+		
+	}
+	
+	
 }
