4c383fc0d94283e83b51925e159fb002d2dfa2f5
Yadnyesh Bharat Juvekar <yadnyesh.juvekar@gmail.com>
2017-05-27 20:15:14 +0530

    Modified Security Utility

--- src/main/java/com/bookstore/config/SecurityUtility.java
+++ src/main/java/com/bookstore/config/SecurityUtility.java
@@ -1,10 +1,46 @@
 package com.bookstore.config;
 
+import java.security.SecureRandom;
+import java.util.Random;
+
+import org.springframework.context.annotation.Bean;
+import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
 import org.springframework.stereotype.Component;
 
 @Component
 public class SecurityUtility {
 	
 	private static final String SALT = "salt"; // should be protected carefully
+	
+	@Bean
+	public static BCryptPasswordEncoder passwordEncoder() {
+		return new BCryptPasswordEncoder (12, new SecureRandom(SALT.getBytes()));
+	}
+	
+	public static String randomPassword() {
+		String SALTCHARS = "ABCDEFGHIJKLMNOPQRSTUVWXYZ1234567890";
+		StringBuilder salt = new StringBuilder();
+		Random rnd = new Random();
+		
+		while(salt.length() < 18) {
+			int index = (int) (rnd.nextFloat() * SALTCHARS.length());
+			salt.append(SALTCHARS.charAt(index));
+		}
+		
+		String saltstr = salt.toString();
+		return saltstr;
+	}
+	
+	
+	
+	
+	
+	
+	
+	
+	
+	
+	
+	
 
 }
