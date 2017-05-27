79a0faa4102e2e310a890ff0c33b71f10e5bf0bb
Yadnyesh Bharat Juvekar <yadnyesh.juvekar@gmail.com>
2017-05-27 16:44:18 +0530

    Test

--- src/main/java/com/bookstore/domain/security/UserRole.java
+++ src/main/java/com/bookstore/domain/security/UserRole.java
@@ -3,6 +3,8 @@ package com.bookstore.domain.security;
 import java.io.Serializable;
 
 import javax.persistence.Entity;
+import javax.persistence.GeneratedValue;
+import javax.persistence.GenerationType;
 import javax.persistence.Id;
 import javax.persistence.Table;
 
@@ -12,7 +14,21 @@ public class UserRole implements Serializable{
 
 	private static final long serialVersionUID = 800000L;
 	
+	private User user;
+	
+	private Role role;
+	
 	@Id
+	@GeneratedValue(strategy = GenerationType.AUTO)
 	private Long userRoleId;
+	
+	public UserRole(){}
+
+	public UserRole(Long userRoleId) {
+		super();
+		this.userRoleId = userRoleId;
+	}
+	
+	
 	
 }
