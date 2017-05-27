7bccd99396f47c53fb093ca683e6c3819da7f51e
Yadnyesh Bharat Juvekar <yadnyesh.juvekar@gmail.com>
2017-05-28 02:32:28 +0530

    Updated Session config file

--- src/main/java/com/bookstore/config/HttpSessionConfig.java
+++ src/main/java/com/bookstore/config/HttpSessionConfig.java
@@ -1,8 +1,12 @@
 package com.bookstore.config;
 
+import org.springframework.data.redis.connection.lettuce.LettuceConnectionFactory;
 import org.springframework.session.data.redis.config.annotation.web.http.EnableRedisHttpSession;
 
 @EnableRedisHttpSession
 public class HttpSessionConfig {
 
+	public LettuceConnectionFactory connectionFactory() {
+		
+	}
 }
