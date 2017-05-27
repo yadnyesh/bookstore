adb42c3cedc8d361d5072238756f3479534a17b4
Yadnyesh Bharat Juvekar <yadnyesh.juvekar@gmail.com>
2017-05-27 16:31:35 +0530

    First Commit

---
+++ src/main/java/com/bookstore/domain/security/Role.java
@@ -0,0 +1,50 @@
+package com.bookstore.domain.security;
+
+import java.io.Serializable;
+
+import javax.persistence.Entity;
+import javax.persistence.Id;
+
+@Entity
+public class Role implements Serializable{
+	
+	/**
+	 * 
+	 */
+	private static final long serialVersionUID = 134432L;
+
+	@Id
+	private int roleId;
+	
+	private String name;
+	
+	private Set<UserRole> userRoles = new HashSet<>();
+	
+	public Role() {}
+
+	public int getRoleId() {
+		return roleId;
+	}
+
+	public void setRoleId(int roleId) {
+		this.roleId = roleId;
+	}
+
+	public String getName() {
+		return name;
+	}
+
+	public void setName(String name) {
+		this.name = name;
+	}
+
+	public Set<UserRole> getUserRoles() {
+		return userRoles;
+	}
+
+	public void setUserRoles(Set<UserRole> userRoles) {
+		this.userRoles = userRoles;
+	}
+	
+	
+}
