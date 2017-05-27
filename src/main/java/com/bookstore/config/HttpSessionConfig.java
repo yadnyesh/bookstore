bdd9e2ad9ef2e8cf7c905a0d1c3277fa189516cb
Yadnyesh Bharat Juvekar <yadnyesh.juvekar@gmail.com>
2017-05-28 02:31:51 +0530

    Added Session config file

---
+++ src/main/java/com/bookstore/config/HttpSessionConfig.java
@@ -0,0 +1,8 @@
+package com.bookstore.config;
+
+import org.springframework.session.data.redis.config.annotation.web.http.EnableRedisHttpSession;
+
+@EnableRedisHttpSession
+public class HttpSessionConfig {
+
+}
