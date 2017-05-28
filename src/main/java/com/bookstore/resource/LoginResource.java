2b3b6f869e9de7eac65fb751cf522a2f20e0f395
Yadnyesh Bharat Juvekar <yadnyesh.juvekar@gmail.com>
2017-05-28 15:27:19 +0530

    Update login service

--- src/main/java/com/bookstore/resource/LoginResource.java
+++ src/main/java/com/bookstore/resource/LoginResource.java
@@ -10,6 +10,7 @@ import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestMethod;
 import org.springframework.web.bind.annotation.RestController;
 
 import com.bookstore.service.UserService;
@@ -37,7 +38,7 @@ public class LoginResource {
 		return new ResponseEntity("Session Active!", HttpStatus.OK);
 	}
 	
-	
+	@RequestMapping(value="/logout", method=RequestMethod.POST)
 	public ResponseEntity logout(){
 		
 	}
