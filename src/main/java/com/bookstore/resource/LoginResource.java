48af5efa18dca0acacd3f37d36eed9dd1dfd0af9
Yadnyesh Bharat Juvekar <yadnyesh.juvekar@gmail.com>
2017-05-28 15:28:34 +0530

    Update login service

--- src/main/java/com/bookstore/resource/LoginResource.java
+++ src/main/java/com/bookstore/resource/LoginResource.java
@@ -9,6 +9,7 @@ import javax.servlet.http.HttpSession;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
+import org.springframework.security.core.context.SecurityContextHolder;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestMethod;
 import org.springframework.web.bind.annotation.RestController;
@@ -40,7 +41,8 @@ public class LoginResource {
 	
 	@RequestMapping(value="/logout", method=RequestMethod.POST)
 	public ResponseEntity logout(){
-		
+		SecurityContextHolder.clearContext();
+		return new ResponseEntity("Logout Successful", HttpStatus.OK);
 	}
 	
 }
