5f8298bc32151ed38c31d1dfe47a90fd10c18871
Yadnyesh Bharat Juvekar <yadnyesh.juvekar@gmail.com>
2017-05-28 14:25:24 +0530

    Update LoginResource

--- src/main/java/com/bookstore/resource/LoginResource.java
+++ src/main/java/com/bookstore/resource/LoginResource.java
@@ -2,6 +2,8 @@ package com.bookstore.resource;
 
 import org.springframework.web.bind.annotation.RestController;
 
+import com.bookstore.domain.User;
+
 @RestController
 public class LoginResource {
 
