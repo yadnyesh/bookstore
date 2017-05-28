85c9658d0a59cd57d0739f8352c99c0ef03045d6
Yadnyesh Bharat Juvekar <yadnyesh.juvekar@gmail.com>
2017-05-28 14:58:00 +0530

    Update LoginResource

--- src/main/java/com/bookstore/resource/LoginResource.java
+++ src/main/java/com/bookstore/resource/LoginResource.java
@@ -7,6 +7,7 @@ import javax.servlet.http.HttpServletRequest;
 import javax.servlet.http.HttpSession;
 
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
@@ -32,7 +33,7 @@ public class LoginResource {
 	}
 	
 	public ResponseEntity checkSession() {
-		
+		return new ResponseEntity("Session Active!", HttpStatus.OK);
 	}
 	
 }
